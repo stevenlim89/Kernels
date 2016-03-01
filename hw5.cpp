97733608590b482ffb10beb325b6426268076d33
Steven Lim <stevenlim890@gmail.com>
2016-02-29 21:09:52 -0800

    finished I/O stuff

--- hw5.cpp
+++ hw5.cpp
@@ -7,26 +7,73 @@
 #include <queue>
 #include <cstdio>
 #include <sstream>
-#include "node.cpp"
 #include <iomanip>
+#include <string>
 
-vector< vector<double> > fileToMatrix(string filename){
+using namespace std;
+
+typedef pair< vector<char>, int> Pair;
+
+vector<int> vectorAdd(vector<int> v1, vector<int> v2){
+    vector<int> sum(784, 0);
+    for(int i = 0; i < 784; i++){
+        sum[i] = v1[i] + v2[i];
+    }
+    return sum;
+}
+
+vector<int> vectorMul(vector<int> v1, int scalar){
+    vector<int> mul(784, 0);
+    for(int i = 0; i < 784; i++){
+        mul[i] = v1[i] * scalar;
+    }
+    return mul;
+}
+
+int labelSet(int target, int currLabel){
+    if(currLabel == target){
+        return 1;
+    }
+    else{
+        return -1;
+    } 
+}
+
+vector<int> perceptron(vector< vector<int> > matrix, int passes, int target){
+    vector<int> w(784, 0);
+    int dot_product = 0;
+    int label = 0;
+
+    for(int p = 0; p < passes; p++){
+        for(int i = 0; i < matrix.size(); i++){
+            label = labelSet(target, matrix[i][784]);
+            dot_product = inner_product(w.begin(), w.end(), matrix[i].begin(), 0);
+
+            if(dot_product*label <= 0){
+                w = vectorAdd(w, vectorMul(matrix[i], label)); 
+            }
+        }
+    }
+    return w;
+}
+
+vector< pair< vector<char>, int> > fileToMatrix(string filename){
     char delimiter = ' ';
 
-    // feature vector
-    vector<double> feature;
-
-    // Matrix to hold all feature vecttors
-    vector< vector<double> > featureMatrix;
-
     // Variable to hold char number from file
-    char line[4];
+    char line[1000];
 
+    char label[2];
     // Variable to hold number that used to be a char
-    double number;
+    int number;
 
-    // Counter to check when to put into featureMatrix
-    int count = 0;
+    string s;
+
+    pair< vector<char>, int> p;
+
+    vector< pair< vector<char>, int> > returnVector;
+
+    vector<char> letters;
 
     ifstream trainfile; 
     trainfile.open(filename);
@@ -34,43 +81,45 @@ vector< vector<double> > fileToMatrix(string filename){
     //check to see if the file is opened:
     if (trainfile.is_open())
     {
-        //while there are still lines in the
-        //file, keep reading:
-        while (! trainfile.eof() )
+        while ( trainfile.getline(line, 1000) )
         {
-            count++;
+            for(int i = 0; i < 1000; i++){
+                letters.push_back(line[i]);
+                if(line[i] == ' '){
+                    letters.pop_back();
+                    if(line[i+2] == '-'){
+                        number = -1;
+                    }
+                    else{
+                        number = 1;
+                    }
+                    p.first = letters;
+                    p.second = number;
+                    returnVector.push_back(p);
+                    break;
+                }
+            }
 
-            // Get character from file and store in variable line
-            trainfile.getline(line, 256, delimiter);
-
-            // line is a character so convert to int
-            number = atof(line);
-
-            // add to feature vector
-            feature.push_back(number);
-
-            // If counter is 5, then feature vector has been filled and you can add it to matrix
-            if(count == 5){
-                featureMatrix.push_back(feature);
-
-                // reset values
-                count = 0;
-                feature.clear();
-            } 
-
+            letters.clear();
         }     
     }
+
     trainfile.close();
 
-    return featureMatrix;
+    return returnVector;
 }
 
 int main() {
     string trainingFile = "hw5train.txt";
     string testFile = "hw5test.txt";
 
-    vector< vector<double> > trainingMatrix = fileToMatrix(trainingFile);
-    vector< vector<double> > testMatrix = fileToMatrix(testFile);
+    vector< pair< vector<char>, int> > trainingVector = fileToMatrix(trainingFile);
+    cout<<"in main"<<endl;
+    //vector< pair< vector<char>, int> > testVector = fileToMatrix(testFile);
+
+    for(int i = 0; i < trainingVector.size(); i++){
+        //cout<<trainingVector[i].first[trainingVector[i].first.size()-1]<<endl;
+    }
    
     return 0;
 }
